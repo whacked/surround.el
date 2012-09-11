e2124f520147869d1a9e67b46a49eefaf3e111ad
natto <natto@natto-tp.(none)>
2012-09-11 02:50:30 -0400

    (brittle!) extend to support change surround + delete surround via select menu

--- surround.el
+++ surround.el
@@ -22,7 +22,20 @@
 ;; -- natto 2012-07-16 23:47:46
 ;;
 ;; do something like this after load
-;; (global-set-key "\C-cs" 'surround-change-wrap)
+;; (global-set-key "\C-cs" 'surround-choose-operation)
+
+(defun surround-choose-operation ()
+  (interactive)
+  (let* ((opch (char-to-string (read-char "operation: [c]hange [d]elete")))
+           )
+      (cond ((string= "c" opch)
+             (surround-change-wrap))
+            ((string= "d" opch)
+             (surround-delete-wrap))
+            (t
+             (message (concat "unsupported: " opch))))
+      )
+  )
 
 (defun surround-change-wrap ()
   (interactive)
@@ -47,4 +60,28 @@
         )
       ))
   )
+
+(defun surround-delete-wrap ()
+  (interactive)
+  (flet ((get-match (R)
+                    (cond ((string= R ")") "(")
+                          ((string= R "]") "[")
+                          ((string= R "}") "{")
+                          (t R))))
+    (let ((opch (char-to-string (read-char "dewrap: ' \" ) ] } > or [i]nside [a]round"))))
+      (let* ((wrap-R (if (search opch "ia")
+                         (char-to-string (read-char "dewrap: ' \" ) ] } >"))
+                       opch))
+             (wrap-L (get-match wrap-R))
+             )
+        (save-excursion
+          (forward-char)
+          (search-backward wrap-L)
+          (if (string= opch "i") (forward-char)
+            (delete-char 1))
+          (if (search opch "ia") (zap-to-char 1 (string-to-char wrap-R))
+            (search-forward wrap-R))
+          (if (string= opch "i") (insert wrap-R)
+            (backward-delete-char 1))
+          )))))
 
